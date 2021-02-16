5a794e73b6fa12fa360a0646c2bf1bd2d9c4888d
niveven <niveven@gmail.com>
2021-02-16 19:45:01 +0200

    new UART files

---
+++ ComponentsCode/Serial_comm_Arduino_Code/Serial_comm_Arduino_Code.ino
@@ -0,0 +1,150 @@
+#include <SoftwareSerial.h>
+#include "RedMP3.h"
+
+/////////////////////////////////////////////////////
+//Variables and definitions for playing MP3 files:
+/////////////////////////////////////////////////////
+
+#define MP3_RX 4 //RX of Serial MP3 module connect to D7 of Arduino
+#define MP3_TX 5 //TX to D8, note that D8 can not be used as RX on Mega2560, you should modify this if you donot use Arduino UNO
+MP3 mp3(MP3_RX, MP3_TX);
+
+int8_t volume = 0x1a;     //0~0x1e (30 adjustable level)
+int8_t folderName = 0x01; //folder name must be 01 02 03 04 ...
+int8_t fileName = 0;      // prefix of file name must be 001xxx 002xxx 003xxx 004xxx ...
+
+////////////////////////////////////////////////////
+// Pins Used in the Arduino:
+////////////////////////////////////////////////////
+
+// Sensor pins:
+int MAGNETIC_PIN_INPUT_COFFEE = 8;
+int MAGNETIC_PIN_INPUT_TEA = 9;
+int FLYING_FISH_INPUT_OUTER_BOX = 10;
+
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+
+// software serial #1: RX = 7(only for syntax), TX = digital 6
+SoftwareSerial portDebug(7, 6);
+
+// input as string *from* ESP:
+String string;
+
+// output *to* ESP:
+int transmit = 0;
+int old_transmit = -1;
+
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+
+void setup()
+{
+  pinMode(MAGNETIC_PIN_INPUT_COFFEE, INPUT);
+  pinMode(MAGNETIC_PIN_INPUT_TEA, INPUT);
+  pinMode(FLYING_FISH_INPUT_OUTER_BOX, INPUT);
+
+
+  Serial.begin(9600);
+  portDebug.begin(9600);
+
+}
+
+/////////////////////////////////////////////////
+
+void loop()
+{
+  // Getting info about the physical state of the sensors:
+  int state_COFFEE = digitalRead(MAGNETIC_PIN_INPUT_COFFEE);
+  int state_TEA = digitalRead(MAGNETIC_PIN_INPUT_TEA);
+  int state_OUTER_BOX = digitalRead(FLYING_FISH_INPUT_OUTER_BOX);
+  
+  transmit = state_OUTER_BOX * 100 + state_COFFEE * 10 + state_TEA;
+  if (transmit != old_transmit) {
+    portDebug.print("Sending to ESP, print: ");
+    portDebug.print(transmit);
+    portDebug.print(" , write: ");
+    portDebug.write(transmit);
+    portDebug.println();
+    Serial.print(transmit);
+    delay(1000);
+  }
+  old_transmit = transmit;
+  
+  //Accepting data about sound to play from the ESP:
+  portDebug.println("checking input");
+  if (Serial.available() > 0) {
+    string = Serial.readString();
+    portDebug.println();
+    portDebug.println();
+    portDebug.print("got input from ESP,  print: ");
+    portDebug.print(string);
+    portDebug.println();
+    // Changing sound-filename according to the recieved code:
+    if (string == "10"){
+        portDebug.println("got 10 - Playing Greeting");
+        fileName = 0x01;
+        }
+    else if (string == "11"){
+        portDebug.println("got 11 - Playing Face OK");
+        fileName = 0x02;
+    }
+    else if (string == "12"){
+        portDebug.println("got 12 - Playing Face Fail");
+        fileName = 0x03;
+    }
+    else if (string == "13"){
+        portDebug.println("got 13 - Playing Goodbye");
+        fileName = 0x04;
+    }
+    
+      // Debug prints:
+    
+    else if (string == "1"){
+        portDebug.println("Case 0 in ESP");
+        fileName = 0;
+    }
+    else if (string == "2"){
+        portDebug.println("Case 1 in ESP");
+        fileName = 0;
+    }
+    else if (string == "3"){
+        portDebug.println("Case 2 in ESP");
+        fileName = 0;
+    }
+    else if (string == "4"){
+        portDebug.println("Case 3 in ESP");
+        fileName = 0;
+    }
+    else if (string == "5"){
+        portDebug.println("Case 4 in ESP");
+        fileName = 0;
+    }
+    else if (string == "6"){
+        portDebug.println("Case 5 in ESP");
+        fileName = 0;
+    }
+    else if (string == "7"){
+        portDebug.println("Case 6 in ESP");
+        fileName = 0;
+    }
+    
+    // Playing Sound:
+    if (fileName)
+    {
+      delay(500); //Requires 500ms to wait for the MP3 module to initialize
+      mp3.setVolume(volume);
+      delay(50); //you should wait for >=50ms between two commands
+      mp3.playWithFileName(folderName, fileName);
+      delay(8000);
+      fileName = 0;
+    }
+  }
+  delay(1500);
+}
+
+
+
+/*********************************************************************************************************
+  The end of file
+*********************************************************************************************************/
---
+++ ComponentsCode/Serial_comm_ESP_Code/Serial_comm_ESP_Code.ino
@@ -0,0 +1,431 @@
+#include "WiFi.h"
+#include "FirebaseESP32.h"
+#include "esp_camera.h"
+#include "fd_forward.h"
+#include "fr_forward.h"
+#include "fr_flash.h"
+
+#define relayPin 2 // pin 12 can also be used
+#define ENROLL_CONFIRM_TIMES 5
+#define FACE_ID_SAVE_NUMBER 7
+
+unsigned long currentMillis = 0;
+unsigned long openedMillis = 0;
+long interval = 5000; // open lock for ... milliseconds
+
+////////////////////////////////////////////
+// WiFi parameters:
+////////////////////////////////////////////
+
+const char *ssid = "niv";
+const char *password = "204442321";
+
+////////////////////////////////////////////
+// Pins Used in the ESP:
+////////////////////////////////////////////
+
+///////////////////////////////////////////
+// Initial state of machine sensors:
+///////////////////////////////////////////
+
+int OUTER_BOX_STATE = 0;
+int TEA_BOX_STATE = 0;
+int COFFEE_BOX_STATE = 0;
+
+//////////////////////////////////////////
+// Local variables used in the code:
+/////////////////////////////////////////
+
+// Communicaiton variables:
+String sensors_state;
+  
+// Sensors and states' variables:
+int state = 0;
+int prev_state = -1;
+// Prices for drinks:
+int coffee_price = 1;
+int tea_price = 2;
+int cost = 0;
+
+// Face recognition and DB variables:
+bool is_identified = false;
+bool is_sound_played = false;
+FirebaseData firebaseData;
+int faceRecognitionCounter = 0;
+int faceID_recognized = -1;
+char firebase_credit[256] = "";
+char str_id[256] = "";
+int val = -1;
+
+/////////////////////////////////////////
+/////////////////////////////////////////
+
+void connectWifi()
+{
+  // Let us connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  //Serial.println(".......");
+  //Serial.println("WiFi Connected....IP Address:");
+  //Serial.println(WiFi.localIP());
+}
+
+//////////////////////////////////////////
+
+static inline mtmn_config_t app_mtmn_config()
+{
+  mtmn_config_t mtmn_config = {0};
+  mtmn_config.type = FAST;
+  mtmn_config.min_face = 80;
+  mtmn_config.pyramid = 0.707;
+  mtmn_config.pyramid_times = 4;
+  mtmn_config.p_threshold.score = 0.6;
+  mtmn_config.p_threshold.nms = 0.7;
+  mtmn_config.p_threshold.candidate_number = 20;
+  mtmn_config.r_threshold.score = 0.7;
+  mtmn_config.r_threshold.nms = 0.7;
+  mtmn_config.r_threshold.candidate_number = 10;
+  mtmn_config.o_threshold.score = 0.7;
+  mtmn_config.o_threshold.nms = 0.7;
+  mtmn_config.o_threshold.candidate_number = 1;
+  return mtmn_config;
+}
+mtmn_config_t mtmn_config = app_mtmn_config();
+
+//////////////////////////////////////////////
+
+static face_id_list id_list = {0};
+dl_matrix3du_t *image_matrix = NULL;
+camera_fb_t *fb = NULL;
+
+dl_matrix3du_t *aligned_face = dl_matrix3du_alloc(1, FACE_WIDTH, FACE_HEIGHT, 3);
+
+int rzoCheckForFace()
+{
+  currentMillis = millis();
+  int faceID_recognized = run_face_recognition();
+  if (faceID_recognized >= 0)
+  { // face recognition function has returned true
+    //Serial.println("Face recognised");
+    digitalWrite(relayPin, HIGH); //close (energise) relay
+    openedMillis = millis();      //time relay closed
+  }
+  if (currentMillis - interval > openedMillis)
+  {                              // current time - face recognised time > 5 secs
+    digitalWrite(relayPin, LOW); //open relay
+  }
+  return faceID_recognized;
+}
+
+///////////////////////////////////////////////////////
+
+int run_face_recognition()
+{
+  bool faceRecognised = false; // default
+  int64_t start_time = esp_timer_get_time();
+  fb = esp_camera_fb_get();
+  if (!fb)
+  {
+    //Serial.println("Camera capture failed");
+    return -1;
+  }
+
+  int64_t fb_get_time = esp_timer_get_time();
+  //Serial.printf("Get one frame in %u ms.\n", (fb_get_time - start_time) / 1000); // this line can be commented out
+
+  image_matrix = dl_matrix3du_alloc(1, fb->width, fb->height, 3);
+  uint32_t res = fmt2rgb888(fb->buf, fb->len, fb->format, image_matrix->item);
+  if (!res)
+  {
+    //Serial.println("to rgb888 failed");
+    dl_matrix3du_free(image_matrix);
+  }
+
+  esp_camera_fb_return(fb);
+
+  box_array_t *net_boxes = face_detect(image_matrix, &mtmn_config);
+
+  int matched_id = -1;
+
+  if (net_boxes)
+  {
+    if (align_face(net_boxes, image_matrix, aligned_face) == ESP_OK)
+    {
+
+      matched_id = recognize_face(&id_list, aligned_face);
+      if (matched_id >= 0)
+      {
+      //  Serial.printf("Match Face ID: %u\n", matched_id);
+        faceRecognised = true; // function will now return true
+      }
+      else
+      {
+        //Serial.println("No Match Found");
+        matched_id = -1;
+      }
+    }
+    else
+    {
+      //Serial.println("Face Not Aligned");
+    }
+
+    free(net_boxes->box);
+    free(net_boxes->landmark);
+    free(net_boxes);
+  }
+
+  dl_matrix3du_free(image_matrix);
+  return matched_id;
+}
+
+////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+void setup()
+{
+  
+  Serial.begin(9600, SERIAL_8N1);
+  connectWifi();
+  Firebase.begin("https://coffeeiot-c846f.firebaseio.com", "ZJqbWyM3KpiLSk7zLaPWC06JEnfsbT5bDov0Zr7K");
+
+  digitalWrite(relayPin, LOW);
+  pinMode(relayPin, OUTPUT);
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = 5;
+  config.pin_d1 = 18;
+  config.pin_d2 = 19;
+  config.pin_d3 = 21;
+  config.pin_d4 = 36;
+  config.pin_d5 = 39;
+  config.pin_d6 = 34;
+  config.pin_d7 = 35;
+  config.pin_xclk = 0;
+  config.pin_pclk = 22;
+  config.pin_vsync = 25;
+  config.pin_href = 23;
+  config.pin_sscb_sda = 26;
+  config.pin_sscb_scl = 27;
+  config.pin_pwdn = 32;
+  config.pin_reset = -1;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_SVGA;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK)
+  {
+    //Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  //drop down frame size for higher initial frame rate
+  sensor_t *s = esp_camera_sensor_get();
+  s->set_framesize(s, FRAMESIZE_QVGA);
+
+  face_id_init(&id_list, FACE_ID_SAVE_NUMBER, ENROLL_CONFIRM_TIMES);
+  read_face_id_from_flash(&id_list); // Read current face data from on-board flash
+}
+
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+
+void loop()
+{
+  
+ // Reading the sensores state from Arduino:
+    if(Serial.available() > 0){
+      sensors_state = Serial.readString();
+      
+      //Debug prints:
+      //Serial.print(sensors_state);
+     // Serial.println();
+      //End of debug prints
+      
+      if (sensors_state == "0"){
+       // Serial.println("inside IF 0");
+        TEA_BOX_STATE = 0;
+        COFFEE_BOX_STATE = 0;
+        OUTER_BOX_STATE = 0;
+      }
+      if (sensors_state == "1" || sensors_state == "11" || sensors_state == "101" || sensors_state == "111"){ 
+      //  Serial.println("inside IF 1");
+        TEA_BOX_STATE = 1;}
+      if (sensors_state == "10" || sensors_state == "11" || sensors_state == "110" || sensors_state ==  "111"){
+        //Serial.println("inside IF 2");
+        COFFEE_BOX_STATE = 1;} 
+      if (sensors_state == "100"|| sensors_state == "101" || sensors_state == "110" || sensors_state == "111"){
+        //Serial.println("inside IF 3");
+        OUTER_BOX_STATE = 1;}
+    //Serial.println();
+    }
+ 
+  // Machine logic according to states:
+  switch (state)
+  {
+    // Case 0: Standby mode, Case is closed, Waiting for a user to open the lid:
+  case 0:
+  //  Serial.println("CASE 0");
+    if (state != prev_state){
+      Serial.print(1);
+      delay(3000);
+      prev_state = state;
+    }
+    if (OUTER_BOX_STATE == 1)
+    {
+      state = 1;
+    }
+    break;
+
+    // Case 1: Outer lid is opened by a user, a voice prompt is sent:
+  case 1:
+    if (state != prev_state){
+      Serial.print(2);
+      delay(3000);
+      prev_state = state;
+    }
+    state = 2;
+    //PLAY GREETING SOUND:
+    Serial.print(10);
+    delay(3000);
+    break;
+
+    // Case 2: Outer lid is open & the voice propmt was made, starting voice recognition attempts(Face recognised - move to case #3, 20 failed attempts - back to state #1):
+  case 2:
+    if (state != prev_state){
+      Serial.print(3);
+      delay(3000);
+      prev_state = state;
+    }
+    if (faceRecognitionCounter == 20)
+    {
+      //PLAY "FACE_FAIL" SOUND:
+      Serial.print(12);
+      delay(3000);
+      //Change state back to begining (state still = 2):
+      faceRecognitionCounter = 0;
+      break;
+    }
+    faceRecognitionCounter += 1;
+    faceID_recognized = rzoCheckForFace();
+    //faceID_recognized = 1;
+    if (faceID_recognized >= 0)
+    {
+      str_id[0] = 0;
+      // Play "Face OK":
+      Serial.print(11);
+      delay(3000);
+      //change state:
+      state = 3;
+    }
+    break;
+
+    // Case 3: The User face was recognised, waiting for coffee/tea box to be opened:
+  case 3:
+    if (state != prev_state){
+      Serial.print(4);
+      delay(3000);
+      prev_state = state;
+    }
+    cost = 0;
+    if (TEA_BOX_STATE == 1 || COFFEE_BOX_STATE == 1)
+    {
+      if (TEA_BOX_STATE == 1)
+      {
+        cost += tea_price;
+        state = 4;
+        break;
+      }
+      if (COFFEE_BOX_STATE == 1)
+      {
+        cost += coffee_price;
+        state = 5;
+        break;
+      }
+    }
+    break;
+
+    // Case 4: Recognised that the coffee box was opened, updating the bill for the user:
+  case 4:
+    if (state != prev_state){
+      Serial.print(5);
+      delay(3000);
+      prev_state = state;
+    }
+  //  Serial.println("CASE 4");
+    if (COFFEE_BOX_STATE == 1)
+    {
+      cost += coffee_price;
+      state = 6;
+      delay(5000);
+      break;
+    }
+    else if (OUTER_BOX_STATE == 0)
+    {
+      state = 6;
+      delay(5000);
+      break;
+    }
+    break;
+
+    // Case 5: Recognised that the tea box was opened, updating the bill for the user:
+  case 5:
+    if (state != prev_state){
+      Serial.print(6);
+      delay(3000);
+      prev_state = state;
+    }
+    if (TEA_BOX_STATE == 1)
+    {
+      cost += tea_price;
+      state = 6;
+      delay(5000);
+      break;
+    }
+    else if (OUTER_BOX_STATE == 0)
+    {
+      state = 6;
+      delay(5000);
+      break;
+    }
+    break;
+
+    // Case 6: User finished purchase - update the DB:
+  case 6: //Charged customer in firebase
+    if (state != prev_state){
+      Serial.print(7);
+      delay(3000);
+      prev_state = state;
+    }
+   // Serial.println("CASE 6");
+    if (Firebase.getInt(firebaseData, str_id))
+    {
+      if (firebaseData.dataType() == "int")
+      {
+        val = firebaseData.intData();
+        if (val > 0)
+        {
+          firebase_credit[0] = 0;
+          firebase_credit[0] = 0;
+          Firebase.setInt(firebaseData, str_id, val - cost);
+          state = 0;
+        }
+        faceRecognitionCounter = 0;
+      }
+    }
+    //PLAY GOODBYE SOUND:
+    Serial.print(13);
+    delay(2000);
+    state=0;
+    break;
+    
+  }
+}
