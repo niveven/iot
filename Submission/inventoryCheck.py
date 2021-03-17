871efcf70bba543821f40b01096b98c2a00b9d26
noac93 <noac93@gmail.com>
2021-03-17 08:26:36 +0200

    Added users debt mail

--- Submission/inventoryCheck.py
+++ Submission/inventoryCheck.py
@@ -9,15 +9,18 @@ import requests
 sender_address = 'trustcafeiot@gmail.com'
 sender_pass = 'trustcafe'
 receiver_address = 'trustcafeiot@gmail.com'
+base_url = 'https://coffeeiot-c846f.firebaseio.com/'
+headers = {
+    'Content-type': 'application/json',
+}
 
-
-def send_mail(item, count):
+def send_mail(subject, mail_content):
     message = MIMEMultipart()
     message['From'] = sender_address
     message['To'] = receiver_address
-    message['Subject'] = f'Inventory Alert! {item} is about to run out'
-    mail_content = f'There are {str(count)} {item} left. Please purchase more'
-    message.attach(MIMEText(mail_content, 'plain'))
+    message['Subject'] = subject
+    content =  mail_content
+    message.attach(MIMEText(mail_content, 'html'))
     session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
     session.starttls() #enable security
     session.login(sender_address, sender_pass) #login with mail_id and password
@@ -25,19 +28,43 @@ def send_mail(item, count):
     session.sendmail(sender_address, receiver_address, text)
     session.quit()
 
+
+def send_inventory_mail(item, count):
+    send_mail(f'Inventory Alert! {item} is about to run out', f'There are only <b>{str(count)} {item}</b> left. Please purchase more<br />')
+
+    
+def check_coffee_tea_count():
+    coffee_count_url = base_url + '-20.json?print=pretty' 
+    coffee_count = requests.get(coffee_count_url).json()
+
+    tea_count_url = base_url + '-10.json?print=pretty' 
+    tea_count = requests.get(tea_count_url).json()
+
+    if coffee_count < 50:
+        send_inventory_mail("coffee", coffee_count)
+
+    if tea_count < 50:
+        send_inventory_mail("tea", tea_count)
+
+
+def check_users():
+    users_debt = {}
+    users_legend_url = base_url + 'Users legend.json?print=pretty'
+    users_json = requests.get(users_legend_url).json()
+    users_list = [name for name_mail in users_json for name in name_mail.keys()]
+    for i in range(0,len(users_list)):
+        current_user_url = f'{base_url}{i}.json'
+        current_user_money = requests.get(current_user_url).json()
+        if current_user_money < 0:
+            users_debt.update({users_list[i]: current_user_money})
+    mail_content = f'The following users currently have debts:<br />'
+    for user, money in users_debt.items():
+        mail_content += f'User <b>{user}</b> owns {abs(money)} shekels<br />'
+    send_mail('Users Debt', mail_content)
+
+
 if __name__ == "__main__":
-    headers = {
-        'Content-type': 'application/json',
-    }
+    check_coffee_tea_count();
+    check_users();
 
-    coffeeCountUrl = 'https://coffeeiot-c846f.firebaseio.com/-10.json?print=pretty'
-    coffeeCount = requests.get(coffeeCountUrl).json()
-
-    teaCountUrl = 'https://coffeeiot-c846f.firebaseio.com/-20.json?print=pretty'
-    teaCount = requests.get(teaCountUrl).json()
-
-    if coffeeCount < 10:
-        send_mail("coffee", coffeeCount)
-
-    if teaCount < 10:
-        send_mail("tea", teaCount)
+    
